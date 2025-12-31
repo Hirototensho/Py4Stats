0f08eef28ee9e4164ecc17cecbc2ce18583f908b
Hirototensho <h.tensyo@gmail.com>
2025-12-31 15:33:45 +0900

    setup.py の誤植を修正

--- py4stats/__init__.py
+++ py4stats/__init__.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 from py4stats import *
-# from regression_tools import *
-# from eda_tools import *
 
 __all__ = ['load']
 
 __author__ = 'Hiroto Tensho'
 __version__ = '0.0.1'
-__copyright__ = 'Copyright (c) 2024 Hiroto Tensho'
+__copyright__ = 'Copyright (c) 2025 Hiroto Tensho'
