c3e8471cd96958d16ff65644bcf4b483a7139c1f
Hirototensho <h.tensyo@gmail.com>
2026-01-25 17:50:20 +0900

    pyproject.toml の調整

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 setup(
-    name = 'py4stats',  # パッケージ名（pip listで表示される）
-    version = "0.2.0",  # バージョン
+    name = 'py4stats',                                     # パッケージ名（pip listで表示される）
+    version = "0.3.1",                                     # バージョン
     description = 'simple tools for regression analisys',  # 説明
-    author='Hiroto Tensho',  # 作者名
-    packages = find_packages(),  # 使うモジュール一覧を指定する
-    license='MIT',  # ライセンス
+    author='Hiroto Tensho',                                # 作者名
+    packages = find_packages(),                            # 使うモジュール一覧
+    license = 'MIT',                                       # ライセンス
 )
